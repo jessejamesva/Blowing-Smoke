405ec94758e8fbaa3851e4f773bd4490c8c89e72
jesseJV <jesse.j.vargas@hotmail.com>
2025-10-21 01:56:26 -0400

    prep initial depTest

--- data/traits.sql
+++ data/traits.sql
@@ -1,5 +1,5 @@
-INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Nicholas Pearson', '5', '4', '1', '3', '5');
-INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Shawn Baker', '4', '1', '3', '3', '1');
-INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Donna Mccarthy', '5', '1', '4', '4', '2');
-INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Melanie Hicks', '1', '4', '2', '2', '5');
-INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Kathleen Robinson', '4', '3', '1', '3', '4');
+INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Stephanie Craig', '1', '1', '1', '5', '5');
+INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Jessica Jones', '4', '3', '2', '1', '1');
+INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Brian Tran MD', '3', '5', '4', '5', '1');
+INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Kim Holder', '4', '3', '4', '3', '3');
+INSERT INTO traits (employee_name, Leadership, Teamwork, Creativity, Dependability, Integrity) VALUES ('Douglas Cooper', '1', '1', '2', '1', '3');
